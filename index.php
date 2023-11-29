c95cecdb1581f686eba7b34bb3720737b061e138
Theocharis Apostolidis <theocharisapo@gmail.com>
2023-11-29 15:57:59 +0200

    Implemented: GET /board/my_ships/, GET /board/enemy/

--- index.php
+++ index.php
@@ -9,15 +9,15 @@ $info = [$servername, $username,  $password, $dbname];
 #$user = authenticate(get_player_usernames());
 $user = 'Player1';
 
-#$method = $_SERVER['REQUEST_METHOD'];
-$method = 'GET';
+$method = $_SERVER['REQUEST_METHOD'];
+#$method = 'GET';
 
 $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
-#$request = array('boards');
+#$request = array('board', 'enemy');
 
 $conn = db_connect($info);
 
-switch ($r = array_shift($request)) {
+switch ($r = $request[0]) {
 case 'boards':
 	if ($method == 'GET') {
 		get_boards($conn, $user);
@@ -28,19 +28,23 @@ case 'boards':
 	break;
 case 'board':
 	if ($request[1] == 'my_ships') {
-		if ($request == 'GET') {
-			if ($request.count == 2) {
-				get_ships($conn, $user);
+		if ($method == 'GET') {
+			if (sizeof($request) == 2) {
+				$board = get_board($conn, $user, 'my_ships');
+				header("Content-Type: application/json");
+				echo $board . "\n";
 			} else {
 				get_ship($conn, $user, $request[2]);
 			}
-		} else if ($request == 'POST') {
+		} else if ($method == 'POST') {
 			set_ship($conn, $user, $request[2], $request[3], $request[4], $request[5]);
 		}
 	} else if ($request[1] == 'enemy') {
-		if ($request == 'GET') {
-			if ($request.count == 2) {
-				get_enemy($conn, $user);
+		if ($method == 'GET') {
+			if (sizeof($request) == 2) {
+				$board = get_board($conn, $user, 'enemy');
+				header("Content-Type: application/json");
+				echo $board . "\n";
 			} else {
 				get_enemy_cell($conn, $user, $request[2], $request[3]);
 			}
@@ -56,7 +60,7 @@ case 'status':
 	break;
 case 'players':
 	if ($method == 'GET') {
-		if ($request.count == 1) {
+		if (sizeof($request) == 1) {
 			get_players($conn);
 		} else {
 			get_player($conn, $request[1]);
